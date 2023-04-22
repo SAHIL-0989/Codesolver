b17a682fb0025fb6ad4245170b7c14f6f330853f
jarvis <sahilbarak111@gmail.com>
2023-04-22 11:05:34 +0530

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+// adding different pages in vite
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                Home:'./Home.html',
+                login: './login.html',
+                register: './register.html',
+                solution: './solution.html',
+                bugDetector: './bugDetector.html',
+                beautify: './beautify.html',
+                ContactUs: './ContactUs.html',
+                contactSuccess: './contactSuccess.html',
+                contactFailed: './contactFailed.html',
+                passwordError: './passwordError.html',
+                credentailsError: './credentialsError.html',
+                detailsError: './detailsError.html',
+                emailError: './emailError.html',
+                emailLoginError: './emailLoginError.html',
+                fieldRequiredError: './fieldRequiredError.html',
+                usernameError: './usernameError.html',
+                mainjs: './src/script.js',
+            },
+        },
+    },
+})
